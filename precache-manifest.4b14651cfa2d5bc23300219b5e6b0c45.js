996f38b075128323d0935a4e755dfab71a804cfc
crongle <gasmazk73@gmail.com>
2020-12-03 12:25:36 +0000

    Updates

---
+++ precache-manifest.4b14651cfa2d5bc23300219b5e6b0c45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473fac0aa8348ce529714d8597c38196",
+    "url": "/lister-client/index.html"
+  },
+  {
+    "revision": "1dfd87f4474430cc09ff",
+    "url": "/lister-client/static/css/main.2217905f.chunk.css"
+  },
+  {
+    "revision": "564ee42fdf232a04c017",
+    "url": "/lister-client/static/js/2.9400c24f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lister-client/static/js/2.9400c24f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dfd87f4474430cc09ff",
+    "url": "/lister-client/static/js/main.47707b43.chunk.js"
+  },
+  {
+    "revision": "1d01d62e2cec8d62f0d7",
+    "url": "/lister-client/static/js/runtime-main.26111a60.js"
+  },
+  {
+    "revision": "34280c5916228bcee0b839abb7884f8e",
+    "url": "/lister-client/static/media/add.34280c59.svg"
+  },
+  {
+    "revision": "78aa51f72aed8516d6335f7b9446935b",
+    "url": "/lister-client/static/media/hamburger.78aa51f7.svg"
+  }
+]);
